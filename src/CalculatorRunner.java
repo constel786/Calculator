b61cd605f8f0a2b3151c1f2b82bf6c507de5f7a4
Emre Ozer <constellation2023@gmail.com>
2023-06-04 18:38:50 +0300

    MyMethods

--- src/CalculatorRunner.java
+++ src/CalculatorRunner.java
@@ -1,6 +1,8 @@
 public class CalculatorRunner {
     public static void main(String[] args) {
     Calculator calculator = new Calculator();
-    Calculator add(3,4);
+    Calculator.add(3,4);
+    Calculator.subtract(8,5);
+
     }
 }
