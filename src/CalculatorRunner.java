7ec2284f0868a81b0afa55c9f938938eb2fc6370
Emre Ozer <constellation2023@gmail.com>
2023-06-04 17:56:28 +0300

    RunnerObject

--- src/CalculatorRunner.java
+++ src/CalculatorRunner.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class CalculatorRunner {
+public class CalculatorRunner {
+    public static void main(String[] args) {
+    Calculator calculator = new Calculator ();
+    Calculator add(3,4);
+    }
 }
