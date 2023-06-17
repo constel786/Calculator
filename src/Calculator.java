8d27733177e23c595eff0ff5d2c0afb3cf249588
Emre Ozer <constellation2023@gmail.com>
2023-06-17 20:02:53 +0300

    dividion

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,7 +9,7 @@ public class Calculator {
  public int multiply (int a, int b){
         return a*b;
  }
-public int division (int a, int b){
+public int divide (int a, int b){
         return a/b;
 }
 }
--- src/CalculatorRunner.java
+++ src/CalculatorRunner.java
@@ -4,6 +4,7 @@ public class CalculatorRunner {
     Calculator.add(3,4);
     Calculator.subtract(8,5);
     Calculator.multiply(1,10);
+    Calculator.divide(1,10);
         System.out.println("Calculator is not working");
         System.out.println("calculator = " + calculator);
         System.out.println("Some changes made.");
